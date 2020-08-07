5a5c93986ab94319646df57db0396b97a8117f51
MyFisherTiger <myFT000000>
2020-08-07 16:58:51 +0800

    放弃用命令行调用的方式（太多问题了）

--- medialibrary/src/main/cpp/native-lib.cpp
+++ medialibrary/src/main/cpp/native-lib.cpp
@@ -22,7 +22,7 @@ extern "C" JNIEXPORT jint JNICALL Java_com_kjs_medialibrary_nactive_NativeFFMPEG
       //set java vm
       JavaVM *jvm = NULL;
       env->GetJavaVM(&jvm);
-      av_jni_set_java_vm(jvm, NULL);
+      //av_jni_set_java_vm(jvm, NULL);
 
       char *argCmd[cmdLen] ;
       jstring buf[cmdLen];
@@ -34,7 +34,7 @@ extern "C" JNIEXPORT jint JNICALL Java_com_kjs_medialibrary_nactive_NativeFFMPEG
             LOGD("argCmd=%s",argCmd[i]);
       }
 
-      int retCode = ffmpeg_exec(cmdLen, argCmd);
+      int retCode = 0;//ffmpeg_exec(cmdLen, argCmd);
       LOGD("ffmpeg-invoke: retCode=%d",retCode);
 
       return retCode;
@@ -44,6 +44,6 @@ extern "C" JNIEXPORT jint JNICALL Java_com_kjs_medialibrary_nactive_NativeFFMPEG
  * 获取命令执行进度
  */
 extern "C" JNIEXPORT jint JNICALL Java_com_kjs_medialibrary_nactive_NativeFFMPEG_getProgress(JNIEnv *env, jclass clazz) {
-      int i=get_progress();
+      int i=0;//get_progress();
       return i;
 }
